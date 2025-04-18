477836b84b6960ca3eb59750d730c5360770316c
dobrosi.andras <dobrosi.andras@microsec.hu>
2025-04-19 01:59:40 +0200

    @ManagedOperation

---
+++ src/main/java/com/github/dobrosi/tv2p/configuration/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.github.dobrosi.tv2p.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/actuator/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+            )
+            .httpBasic(httpSecurityHttpBasicConfigurer -> {})
+            .csrf(AbstractHttpConfigurer::disable)
+            .build();
+    }
+}
